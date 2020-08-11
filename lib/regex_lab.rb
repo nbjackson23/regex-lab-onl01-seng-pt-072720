853f9c78f4324119915818f00f91e3ac622ae837
Nancy Jackson <nbjackson23@gmail.com>
2020-08-11 10:29:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,12 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
+  words = text.split(" ")
+  words.grep(/^un\w+ing$/)
 end
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
