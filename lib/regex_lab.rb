57c7207a9a80a24ff8d1b7f222eb16c79c9d374a
Nancy Jackson <nbjackson23@gmail.com>
2020-08-11 10:29:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,11 +12,12 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  
+  words = text.split(" ")
+  words.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
