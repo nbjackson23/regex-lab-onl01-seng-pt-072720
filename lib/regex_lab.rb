ea93105d2331c6f451ea54d01947f5a199125635
Nancy Jackson <nbjackson23@gmail.com>
2020-08-11 10:30:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,9 +17,13 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  
+  if text.scan(/^[A-Z][\s\S]+\.$/).empty?
+    false
+  else
+    true
+  end
 end
 
 def valid_phone_number?(phone)
-
+  
 end
